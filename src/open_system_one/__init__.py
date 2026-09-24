fd3451ba5a65fec3928e11908a31c2c123ee423a
Loofy <ebh.ceo213@gmail.com>
2026-09-24 21:15:08 +0100

    Expose discovery classifier and selector API

--- src/open_system_one/__init__.py
+++ src/open_system_one/__init__.py
@@ -1,9 +1,38 @@
 from .schema import ChoiceQuestion, NoulQuestion, ScoreQuestion, DecisionRequest, DecisionResponse
 from .engine import DecisionEngine, LegacyAdapter
 from .primitives import PRIMITIVES
-from .discovery import propose, coordinate, coordinate_failures
+from .discovery import (
+    FailureObservation,
+    FailureMatch,
+    ExperimentDesign,
+    DiscoveryPlan,
+    propose,
+    coordinate,
+    coordinate_failures,
+    classify_failure,
+    select_experiments,
+    build_plan,
+    build_plans,
+)
 
-__all__=[
-    "ChoiceQuestion","NoulQuestion","ScoreQuestion","DecisionRequest","DecisionResponse",
-    "DecisionEngine","LegacyAdapter","PRIMITIVES","propose","coordinate","coordinate_failures"
+__all__ = [
+    "ChoiceQuestion",
+    "NoulQuestion",
+    "ScoreQuestion",
+    "DecisionRequest",
+    "DecisionResponse",
+    "DecisionEngine",
+    "LegacyAdapter",
+    "PRIMITIVES",
+    "FailureObservation",
+    "FailureMatch",
+    "ExperimentDesign",
+    "DiscoveryPlan",
+    "propose",
+    "coordinate",
+    "coordinate_failures",
+    "classify_failure",
+    "select_experiments",
+    "build_plan",
+    "build_plans",
 ]
